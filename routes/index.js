39d80fa3312d5444f2c32bf66982bdbbfddf3233
Patrick <pvanny1124@gmail.com>
2018-01-29 07:26:16 +0000

    update a lot of stuff

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campgrounds");
 
 //root route
 router.get("/", function(req, res){
@@ -25,7 +26,13 @@ router.post("/register", function(req, res){
      /* eval(require("locus")); */
     //locus stops code so that you can see what variables are available to you. variables have to be declared before eval()
 
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
     if(req.body.adminCode === "secretcode123") newUser.isAdmin = true;
     User.register(newUser, req.body.password, function(err, user){
         if(err){
@@ -37,7 +44,7 @@ router.post("/register", function(req, res){
             req.flash("success", "Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds");
         });
-    });
+    }); 
 });
 
 //show login form
@@ -61,6 +68,27 @@ router.get("/logout", function(req, res){
     req.logout();
     req.flash("success", "Logged you out!");
     res.redirect("/campgrounds");
+});
+
+//USER PROFILE ROUTE
+router.get("/users/:id", function(req, res){
+    //find user
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("/");
+        }
+        console.log(foundUser);
+        //find all campgrounds that is associated to this user
+        Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+            if(err){
+                req.flash("error", err.message);
+                res.redirect("/");
+            }
+            res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+        });
+        
+    });
 });
 
 module.exports = router;
